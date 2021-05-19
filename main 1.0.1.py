99131018d86676ecba3bdb38c6efe902e2ae8a30
nicholaslz <68836624+nicholaslz@users.noreply.github.com>
2021-05-19 12:30:39 -0700

    Update and rename main.py to main 1.0.1.py

---
+++ main 1.0.1.py
@@ -0,0 +1,130 @@
+import json
+
+        
+def main():
+    while True:
+        user_input = input('For sending a message to someone, press 1. For awaiting a message request from someone, press 2.')
+        if user_input == '1' or user_input == '2':
+            if int(user_input) == 1:
+                import socket
+                import sys
+                import time
+
+                socket_server = socket.socket()
+                server_host = socket.gethostname()
+                ip = socket.gethostbyname(server_host)
+                s_port = 8080
+
+                print('This is your IP address: ', ip)
+                server_host = input('Enter friend\'s IP address:')
+                name = input('Enter Friend\'s name: ')
+                 
+
+                try:
+                    socket_se
+rver.connect((server_host, s_port))
+                    socket_server.send(name.encode())
+
+
+                except:
+                    print('Target not available')
+                    exit()
+
+
+                server_name = socket_server.recv(1024)
+                server_name = server_name.decode()
+                    
+                 
+                print(server_name,' has joined...')
+                while True:
+                    try:
+                        message = (socket_server.recv(1024)).decode()
+                        print(server_name, ":", message)
+                        message = input("Me : ")
+                        socket_server.send(message.encode())
+
+                    except ConnectionResetError:
+                        print('The target disconnected')
+                        return
+
+
+            elif int(user_input) == 2:
+                import socket
+                import sys
+                import time
+
+                new_socket = socket.socket()
+                host_name = socket.gethostname()
+                s_ip = socket.gethostbyname(host_name)
+                port = 8080 
+
+                new_socket.bind((host_name, port))
+                print( "Binding successful!")
+                print("This is your IP: ", s_ip)
+                name = input('Enter name:')
+                new_socket.listen(1)
+
+                conn, add= new_socket.accept()
+                print("Received connection from ", add[0])
+                print('Connection Established. Connected From: ',add[0])
+                client = (conn.recv(1024)).decode()
+                print(client + ' has connected.')
+                conn.send(name.encode())
+
+                while True:
+                    try:
+                        message = input('Me : ')
+                        conn.send(message.encode())
+                        message = conn.recv(1024)
+                        message = message.decode()
+                        print(client, ':', message)
+
+                    except ConnectionResetError:
+                        print('The target disconnected')
+                        return
+        else:
+            print('Input not recognized as a valid command')
+            return
+
+
+def init():
+
+    userInput = input('For logging in, press 1, for signing up, press 2')
+
+
+    if int(userInput) == 1:
+        username = input('Please enter your username')
+
+        with open('userinfo.json', 'r+') as f:
+            users = json.load(f)
+
+        if username in users:
+            password = input('Please enter your password')
+            if password == users[username]["Password"]:
+                print('You logged in!')
+                main()
+
+            else:
+                print('Wrong password')
+                exit()
+
+        else:
+            print('Username not recognized')
+            exit()
+
+    elif int(userInput) == 2:
+        username = input('Please set your username')
+        password = input('Please set your password')
+
+        with open('userinfo.json', 'r+') as f:
+            users = json.load(f)
+            
+        users[username] = {'Username': str(username), 'Password': str(password)}
+
+        with open('userinfo.json','r+') as f:
+            json.dump(users, f)
+        
+        main()
+
+init()
+
